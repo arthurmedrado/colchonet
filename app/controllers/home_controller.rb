05cf5d9293730f8e369484287128418effbf0fd3
Thadeu Esteves <tadeuu@gmail.com>
2014-03-10 23:44:59 -0300

    RoomPresenter adicionado em home#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
   	# Busca sem ordem determinada de até 3 quartos
-  	@rooms = Room.most_recent.take(3)
+  	@rooms = Room.most_recent.take(3).map do |room|
+  		RoomPresenter.new(room, self, false)
+  	end
   end
 end
